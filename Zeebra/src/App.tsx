737cb60e03c31eb3ac8c703a167233dfccaaf109
lurufarm <hsg200528@gmail.com>
2025-10-22 11:16:25 +0900

    design: header design

--- Zeebra/src/App.tsx
+++ Zeebra/src/App.tsx
@@ -1,8 +1,13 @@
+import Header from "./common/header/Header";
 
-export default function App() {
+ function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white">
-      <h1 className="text-4xl font-bold">Tailwind v4 + Vite OK</h1>
-    </div>
+    <>
+    <Header/>
+    {/* <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white"> */}
+      {/* <h1 className="text-4xl font-bold">Tailwind v4 + Vite OK</h1> */}
+    {/* </div> */}
+    </>
   );
 }
+export default App;
