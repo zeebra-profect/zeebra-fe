556056f82d3ecc9bd03c9d41ae9081054c86000c
lurufarm <hsg200528@gmail.com>
2025-10-22 12:18:07 +0900

    design: 레이아웃 만들기 + 간단한 route 설정

--- Zeebra/src/App.tsx
+++ Zeebra/src/App.tsx
@@ -1,8 +1,26 @@
 // import Header from "./common/header/Header";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout1 from "./common/layout/Layout1";
+import Layout2 from "./common/layout/Layout2";
+import Main from "./pages/Main";
+
  function App() {
   return (
     <>
+      <BrowserRouter>
+      <Routes>
+        {/* 레이아웃1: 기본 페이지들 */}
+        <Route path="/" element={<Layout1/>}>
+          <Route index element={<Main/>}/>
+        <Route/>
+        {/* 레이아웃2: 마이페이지 내의 페이지들 */}
+        <Route path="/mypage" element={<Layout2/>}>
+        
+        </Route>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
