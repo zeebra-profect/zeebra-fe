653b8718a7ac577c92b0a708b7ca7a19186b350b
lurufarm <hsg200528@gmail.com>
2025-10-22 11:19:28 +0900

    design: header design + 정리

--- Zeebra/src/App.tsx
+++ Zeebra/src/App.tsx
@@ -1,12 +1,8 @@
-import Header from "./common/header/Header";
+// import Header from "./common/header/Header";
 
  function App() {
   return (
     <>
-    <Header/>
-    {/* <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white"> */}
-      {/* <h1 className="text-4xl font-bold">Tailwind v4 + Vite OK</h1> */}
-    {/* </div> */}
     </>
   );
 }
