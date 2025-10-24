6ed4a84526cf32f61caa26dffd149914ffd076cb
Gukbo <39515692+Gukbo@users.noreply.github.com>
2025-10-24 17:52:32 +0900

    design: 로그인 회원가입 페이지 제작 ZBR-73

--- Zeebra/src/App.tsx
+++ Zeebra/src/App.tsx
@@ -3,26 +3,38 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout1 from "./common/layout/Layout1";
 import Layout2 from "./common/layout/Layout2";
+import Layout3 from "./common/layout/Layout3";
 import Main from "./pages/mains/Main";
 import MyPage from "./pages/mains/MyPage";
+import Signup from "./pages/users/Signup";
+import Login from "./pages/users/Login";
+import NotFound from "./pages/etc/NotFound";
 
- function App() {
+function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        {/* 레이아웃1: 기본 페이지들 */}
-        <Route path="/" element={<Layout1/>}>
-          <Route index element={<Main/>}/>
-        <Route/>
-        {/* 레이아웃2: 마이페이지 내의 페이지들 */}
-        <Route path="/mypage" element={<Layout2/>}>
-          <Route index element={<MyPage/>} >
+        <Routes>
+          {/* 공통 레이아웃 */}
+          <Route path="/" element={<Layout1 />}>
+            <Route index element={<Main />} />
 
+            {/* 마이페이지 */}
+            <Route path="mypage" element={<Layout2 />}>
+              <Route index element={<MyPage />} />
+            </Route>
+
+            {/* 로그인/회원가입만 회색 배경 레이아웃3 적용 */}
+            <Route element={<Layout3 />}>
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+            </Route>
+
+            {/* 404 */}
+            <Route path="*" element={<NotFound />} />
           </Route>
-        </Route>
-        </Route>
-      </Routes>
+          
+        </Routes>
       </BrowserRouter>
     </>
   );
--- Zeebra/src/common/layout/Layout1.tsx
+++ Zeebra/src/common/layout/Layout1.tsx
@@ -1,24 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
-import Footer from '../footer/Footer';
+import Footer from "../footer/Footer";
 
 // 기본 레이아웃
 
-function Layout1()
-{
-    return(
-        <>
-            <div className="fixed">
-                <Header></Header>
-            </div>
-            <div className="flex justify-center flex-row font-pretendard pt-[130px]">
-                <div className="w-max-[1200px]">
-                    <Outlet/>
-                </div>
-            </div>
-            <Footer/>
-        </>
-    )
+function Layout1() {
+  return (
+    <>
+      <div className="fixed bg-grey">
+        <Header></Header>
+      </div>
+      <div className="flex justify-center flex-row font-pretendard pt-[130px]">
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Layout1;
