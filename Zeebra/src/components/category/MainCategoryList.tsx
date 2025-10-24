321a865856e9d7ae77cede9b5f7eeb6e10ec8171
lurufarm <hsg200528@gmail.com>
2025-10-24 19:29:18 +0900

    design: 구매 모달 제작 ZBR-84

--- Zeebra/src/components/category/MainCategoryList.tsx
+++ Zeebra/src/components/category/MainCategoryList.tsx
@@ -1,9 +1,9 @@
-import Category from "./category";
+import Category from "./Category";
 
 function MainCategoryList()
 {
     return (
-        <div className="flex flex-row gap-x-4 md:gap-x-6 lg:gap-x-[30px] gap-y-4 md:gap-y-[20px] w-full max-w-[1200px] flex-wrap justify-center md:justify-start">
+        <div className="flex flex-row gap-x-4 md:gap-x-6 lg:gap-x-[30px] gap-y-4 md:gap-y-5 w-full max-w-[1200px] flex-wrap justify-center md:justify-start">
             <Category/>
             <Category/>
             <Category/>
