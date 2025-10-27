248a58258f94f1370067b0fefaa405932dc455fa
lurufarm <hsg200528@gmail.com>
2025-10-27 11:15:48 +0900

    design: 채팅 모달 + 스크롤바 css 수정 ZBR-84

--- Zeebra/src/components/notification/NotificationModal.tsx
+++ Zeebra/src/components/notification/NotificationModal.tsx
@@ -13,13 +13,19 @@ function NotificationModal({ isOpen, onClose, children }: ModalProps) {
     <>
       <div className="fixed inset-0 z-50" onClick={onClose}>
         <div
-          className="absolute right-10 top-2 mt-2 w-96 h-[450px] bg-white shadow-[0_0_5px_rgba(34,34,34,0.3)] rounded-xl text-main-text"
+          className="absolute right-10 top-2 mt-2 w-96 h-auto pl-1 pr-1 bg-white shadow-[0_0_5px_rgba(34,34,34,0.3)] rounded-xl text-main-text"
           onClick={(e) => e.stopPropagation()}
         >
-          <div className="flex flex-col">
+          <div className="flex flex-col h-auto max-h-[450px] overflow-auto scrollbar">
             <div className="flex flex-col font-bold text-lg m-5 items-center justify-center text-center">
               <p className="text-center">알림</p>
             </div>
+            <Notification />
+            <Notification />
+            <Notification />
+            <Notification />
+            <Notification />
+            <Notification />
             <Notification />
             <Notification />
             <Notification />
