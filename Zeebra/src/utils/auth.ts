67e0ecbfe29db1d37e083ef072bcce95538ba467
Gukbo <39515692+Gukbo@users.noreply.github.com>
2025-10-29 09:20:56 +0900

    fix: 로그인 기능 수정
    ZBR-116

---
+++ Zeebra/src/utils/auth.ts
@@ -0,0 +1,16 @@
+import { http } from "./http";
+
+export interface LoginReq {
+  identifier: string;
+  password: string;
+}
+
+export interface LoginRes {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export async function login(req: LoginReq): Promise<LoginRes> {
+  const { data } = await http.post<LoginRes>("/api/auth/login", req);
+  return data;
+}
