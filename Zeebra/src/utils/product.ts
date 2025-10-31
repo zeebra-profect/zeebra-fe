cc07422cd6a78ccec3b53ca79588e5c265ef3afe
lurufarm <hsg200528@gmail.com>
2025-10-31 18:31:35 +0900

    feat: 상품 상세페이지 옵션 연결까지 ZBR-126

--- Zeebra/src/utils/product.ts
+++ Zeebra/src/utils/product.ts
@@ -1,26 +1,52 @@
 import { http } from "./http";
 
 export interface ProductDetail {
-    status: string;
-    message: string;
-    data : {
-        productId: number;
-        brandId: number;
-        categoryId: number;
-        productName: string;
-        productDescription: string;
-        modelNumber: string;
-        productThumbnail: string;
-        images: string[];
-        lowPrice: number;
-        reviewCount: number;
-        favoriteProductCount: number;
-        createdAt: string;
-    };
-    sendTime: string;
+  status: string;
+  message: string;
+  data: {
+    productId: number;
+    brandId: number;
+    categoryId: number;
+    productName: string;
+    productDescription: string;
+    modelNumber: string;
+    productThumbnail: string;
+    images: string[];
+    lowPrice: number;
+    reviewCount: number;
+    favoriteProductCount: number;
+    createdAt: string;
+      colorOptionResponses: Array<
+    {
+      colorOptionNameId: number;
+      colorValue: string;
+    }>;
+    colorValue: string;
+  };
+  sendTime: string;
 }
 
-export async function getProduct(productId : number): Promise<ProductDetail> {
+export interface ProductOption {
+  status: string;
+  message: string;
+  data: {
+    sizeOptionResponses: Array<
+      {
+        optionCombinationId: number;
+        sizeValue: string;
+        productOptionId: number;
+        lowPriceOfSize: number;
+      }>;
+  };
+  sendTime: string;
+}
+
+export async function getProduct(productId: number): Promise<ProductDetail> {
   const { data } = await http.get<ProductDetail>(`/products/${productId}`);
+  return data;
+}
+
+export async function getProductOption(productId: number, colorOptionNameId: number): Promise<ProductOption> {
+  const { data } = await http.get<ProductOption>(`/product-options/${productId}/${colorOptionNameId}`);
   return data;
 }
