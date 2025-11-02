d1737fbb59ac11bd0bbe5de7521b2978dd596a5a
lurufarm <hsg200528@gmail.com>
2025-11-02 15:22:33 +0900

    feat: 그룹챗 웹소켓 로컬 테스트 완료 ZBR-138

--- Zeebra/vite.config.ts
+++ Zeebra/vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    global: 'globalThis',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
