ca75229d9104cbcb4147d4c73d2b3c2db7f2ed10
Gukbo <39515692+Gukbo@users.noreply.github.com>
2025-12-02 12:56:00 +0900

    design: 이미지 변경 ZBR-213

--- Zeebra/src/utils/image.ts
+++ Zeebra/src/utils/image.ts
@@ -1,21 +1,21 @@
 // lorem flickr 적용시
 
-// export const getImageUrl = (path: string | null, productId: number) => {
-//   if (path && path.startsWith("http")) return path;
-
-//   const lockIndex = productId % 100;
-
-//   // 👇 300/300 대신 600/600으로 변경! (화질 깨짐 방지)
-//   return `https://loremflickr.com/600/600/fashion,clothes?lock=${lockIndex}`;
-// };
-
 export const getImageUrl = (path: string | null, productId: number) => {
-  // 1. 만약 DB에 진짜 이미지 주소(http...)가 있다면 그걸 우선 사용
-  console.log(`[이미지확인] ID: ${productId}, Path값:`, path, typeof path);
   if (path && path.startsWith("http")) return path;
 
-  // 2. 이미지가 없다면 Placehold.co 사용
-  // - 600x600: 넉넉한 고화질 사이즈 (CSS object-cover로 맞춤)
-  // - ?text=Product+${productId}: 이미지 한가운데에 상품 번호를 적어줌!
-  return `https://placehold.co/600x600?text=Product+${productId}`;
+  const lockIndex = productId % 3000;
+
+  // 👇 300/300 대신 600/600으로 변경! (화질 깨짐 방지)
+  return `https://loremflickr.com/600/600/fashion,clothes?lock=${lockIndex}`;
 };
+
+// export const getImageUrl = (path: string | null, productId: number) => {
+//   // 1. 만약 DB에 진짜 이미지 주소(http...)가 있다면 그걸 우선 사용
+//   console.log(`[이미지확인] ID: ${productId}, Path값:`, path, typeof path);
+//   if (path && path.startsWith("http")) return path;
+
+//   // 2. 이미지가 없다면 Placehold.co 사용
+//   // - 600x600: 넉넉한 고화질 사이즈 (CSS object-cover로 맞춤)
+//   // - ?text=Product+${productId}: 이미지 한가운데에 상품 번호를 적어줌!
+//   return `https://placehold.co/600x600?text=Product+${productId}`;
+// };
